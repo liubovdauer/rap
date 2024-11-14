8200c18c014fbeca252c1861e68ef4fe14f4f2ce
Liubov Dauer <lyubov.dauer@gmail.com>
2024-11-14 10:59:33 +0100

    add Spring Boot project files

---
+++ src/main/java/de/dauer/rap/RapApplication.java
@@ -0,0 +1,13 @@
+package de.dauer.rap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dauer/rap/RapApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dauer.rap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
