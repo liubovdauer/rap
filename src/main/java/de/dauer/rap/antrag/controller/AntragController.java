f798eb977c312daddc1d8e63776fd1376e4f2c61
Liubov Dauer <lyubov.dauer@gmail.com>
2024-11-22 11:49:33 +0100

    Swagger Ui hinzugefügt

--- src/main/java/de/dauer/rap/antrag/controller/AntragController.java
+++ src/main/java/de/dauer/rap/antrag/controller/AntragController.java
@@ -1,7 +1,11 @@
 package de.dauer.rap.antrag.controller;
 
+import de.dauer.rap.antrag.controller.modell.Antrag;
 import de.dauer.rap.antrag.controller.modell.AntragErgebnis;
 import de.dauer.rap.antrag.controller.modell.Person;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +18,14 @@ public class AntragController {
     }
 
     @PostMapping()
-    public AntragErgebnis antragAnlegen(@RequestBody Person person){
+
+    public ResponseEntity<AntragErgebnis> antragAnlegen(@RequestBody Antrag antrag){
         AntragErgebnis antragErgebnis= new AntragErgebnis();
         antragErgebnis.setAntragId(1L);
         antragErgebnis.setIstGenehmigt(false);
         antragErgebnis.setBusinessPartnerId("AV45GT67");
-       return antragErgebnis ;
+        if (antrag.getPerson()!=null)
+            return ResponseEntity.ok().body(antragErgebnis);
+        return ResponseEntity.badRequest().build();
     }
 }
--- src/main/java/de/dauer/rap/antrag/controller/modell/Antrag.java
+++ src/main/java/de/dauer/rap/antrag/controller/modell/Antrag.java
@@ -2,4 +2,12 @@ package de.dauer.rap.antrag.controller.modell;
 
 public class Antrag {
     private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
